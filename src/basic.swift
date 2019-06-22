ad0c8619661dda4d53910e04396800d394b02a67
张复星 <star@zhangfuxingdeMacBook-Pro.local>
2019-06-22 11:40:32 +0800

    add basic

---
+++ src/basic.swift
@@ -0,0 +1,38 @@
+let maximumNumberOfLoginAttempts = 10
+var currentLoginAttempt = 0
+print("zfx")
+var x = 0.0, y = 0.0, z = 0.0
+
+var welcomeMessage: String
+welcomeMessage = "Hello"
+
+var red, green, blue: Double
+
+let π = 3.14159
+let 你好 = "你好世界"
+let 🐶🐮 = "dogcow"
+
+var friendlyWelcome = "Hello!"
+friendlyWelcome = "Bonjour!"
+// friendlyWelcome is now "Bonjour!"
+
+print("The current value of friendlyWelcome is \(friendlyWelcome)")
+// Prints "The current value of friendlyWelcome is Bonjour!"
+
+// This is a comment.
+/* This is also a comment
+but is written over multiple lines. */
+/* This is the start of the first multiline comment.
+ /* This is the second, nested multiline comment. */
+This is the end of the first multiline comment. */
+
+// 与许多其他语言不同，Swift不要求您;在代码中的每个语句之后编写分号（），但如果您愿意，也可以这样做。但是，如果要在一行上写多个单独的语句，则需要使用分号：
+let cat = "🐱"; print(cat)
+// Prints "🐱"
+
+let minValue = UInt8.min  // minValue is equal to 0, and is of type UInt8
+let maxValue = UInt8.max  // maxValue is equal to 255, and is of type UInt8
+
+print(minValue)
+print(maxValue)
+
