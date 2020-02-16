290b5e2c834c94137be8b46c9d9d9b9f3920a564
张复星 <star@zhangfuxingdeMacBook-Pro.local>
2020-02-16 20:41:45 +0800

    add basicTypes

---
+++ src/playground/BasicTypes.playground/Contents.swift
@@ -0,0 +1,28 @@
+import UIKit
+
+let maximumNumberOfLoginAttempts = 10
+var currentLoginAttempt = 0
+
+    
+var x = 0.0, y = 0.0, z = 0.0
+
+var welcomeMessage: String
+welcomeMessage = "Hello"
+
+var red, green, blue: Double
+
+let π = 3.14159
+let 你好 = "你好世界"
+let 🐶🐮 = "dogcow"
+
+var friendlyWelcome = "Hello!"
+friendlyWelcome = "Bonjour!"
+print(friendlyWelcome)
+// Prints "Bonjour!"
+
+print("The current value of friendlyWelcome is \(friendlyWelcome)")
+// Prints "The current value of friendlyWelcome is Bonjour!"
+
+let languageName = "Swift"
+//languageName = "Swift++"
+// This is a compile-time error: languageName cannot be changed.
